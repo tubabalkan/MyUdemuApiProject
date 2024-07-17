5a31f80d771ce65eaeec40c383a091f8440a9e5c
tuba balkan <tubabalkan165@gmail.com>
2024-07-17 14:19:50 +0300

    ekleme yapıldı

---
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_HeadPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace HotelProject.WebUI.ViewComponents.Default
+{
+    public class _HeadPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
