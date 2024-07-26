09e0d54612eb79dc2aeed1c8249c1af7820e98ca
tuba balkan <tubabalkan165@gmail.com>
2024-07-27 00:15:52 +0300

    ekleme yapıldı

---
+++ ApiConsume/HotelProject.WepApi/Controllers/ContactController.cs
@@ -0,0 +1,28 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WepApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly IContactService _contactservice;
+
+        public ContactController(IContactService contactservice)
+        {
+            _contactservice = contactservice;
+        }
+
+        [HttpPost]
+        public IActionResult AddContact(Contact contact)
+        {
+            contact.Date=Convert.ToDateTime(DateTime.Now.ToString());
+            _contactservice.TInsert(contact);
+            return Ok();
+        }
+       
+    }
+}
---
+++ Frontend/HotelProject.WebUI/ViewComponents/Contact/_ContactCoverPartial.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.ViewComponents.Contact
+{
+    public class _ContactCoverPartial:ViewComponent
+    {
+    }
+}
