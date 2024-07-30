6a63179263a2586f87c0bbe3e6d28b6389056745
tuba balkan <tubabalkan165@gmail.com>
2024-07-30 15:49:57 +0300

    ...........

--- Frontend/HotelProject.WebUI/Controllers/LoginController.cs
+++ Frontend/HotelProject.WebUI/Controllers/LoginController.cs
@@ -1,10 +1,12 @@
 ﻿using HotelProject.EntityLayer.Concrete;
 using HotelProject.WebUI.Dtos.LoginDto;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HotelProject.WebUI.Controllers
 {
+    [AllowAnonymous]
     public class LoginController : Controller
     {
         private readonly SignInManager<AppUser> _signInManager;
@@ -26,7 +28,7 @@ namespace HotelProject.WebUI.Controllers
                 var result=await _signInManager.PasswordSignInAsync(loginUserDto.UserName, loginUserDto.Password,false,false);
                 if(result.Succeeded) 
                 {
-                    return RedirectToAction("Index", "Staff");
+                    return RedirectToAction("Staff","Index");
                 
                 }
                 else
--- Frontend/HotelProject.WebUI/Program.cs
+++ Frontend/HotelProject.WebUI/Program.cs
@@ -5,6 +5,8 @@ using HotelProject.EntityLayer.Concrete;
 using HotelProject.WebUI.Dtos.GuestDto;
 using HotelProject.WebUI.Mapping;
 using HotelProject.WebUI.ValidationRules.GuestValidationRules;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +16,19 @@ builder.Services.AddTransient<IValidator<UpdateGuestDto>,UpdateGuestValidator>()
 builder.Services.AddControllersWithViews().AddFluentValidation();
 builder.Services.AddHttpClient();
 builder.Services.AddAutoMapper(typeof(AutoMappingConfig));
+//builder.Services.AddMvc(config =>
+//{
+//	var policy=new AuthorizationPolicyBuilder()
+//	.RequireAuthenticatedUser()
+//	.Build();
+//	config.Filters.Add(new AuthorizeFilter(policy));
+//});
+//builder.Services.ConfigureApplicationCookie(options =>
+//{
+//	options.Cookie.HttpOnly = true;
+//	options.ExpireTimeSpan = TimeSpan.FromMinutes(10);
+//	options.LoginPath = "/Login/Index";
+//});
 builder.Services.AddDbContext<Context>();
 builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>();
 
@@ -25,13 +40,10 @@ if (!app.Environment.IsDevelopment())
 	app.UseExceptionHandler("/Home/Error");
 }
 app.UseStaticFiles();
-
+//app.UseAuthentication();
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapControllerRoute(
 	name: "default",
 	pattern: "{controller=Home}/{action=Index}/{id?}");
-
 app.Run();
