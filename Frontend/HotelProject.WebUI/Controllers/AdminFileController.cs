058521f7c704b6c51865a1b1662e5c8d2bf8da4f
tuba balkan <tubabalkan165@gmail.com>
2024-07-30 15:14:22 +0300

    ekleme yapıldı

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminFileController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminFileController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile file)
+        {
+            var stream = new MemoryStream();
+            await file.CopyToAsync(stream);
+            var byts = stream.ToArray();
+
+            ByteArrayContent byteArrayContent = new ByteArrayContent(byts);
+            byteArrayContent.Headers.ContentType = new MediaTypeHeaderValue(file.ContentType);
+            MultipartFormDataContent multipartFormDataContent = new MultipartFormDataContent();
+            multipartFormDataContent.Add(byteArrayContent, "file", file.FileName);
+            var httpclient = new HttpClient();
+            var responseMessage = await httpclient.PostAsync("http://localhost:5023/api/FileProcess", multipartFormDataContent);
+
+            return View();
+        }
+    }
+}
---
+++ Frontend/HotelProject.WebUI/Controllers/AdminImageFileController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminImageFileController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile file)
+        {
+            var stream = new MemoryStream();
+            await file.CopyToAsync(stream);
+            var byts=stream.ToArray();
+
+            ByteArrayContent byteArrayContent= new ByteArrayContent(byts);
+            byteArrayContent.Headers.ContentType = new MediaTypeHeaderValue(file.ContentType);
+            MultipartFormDataContent multipartFormDataContent = new MultipartFormDataContent();
+            multipartFormDataContent.Add(byteArrayContent, "file", file.FileName);
+            var httpclient=new HttpClient();
+            var responseMessage = await httpclient.PostAsync("http://localhost:5023/api/FileImage",multipartFormDataContent);
+        
+            return View();
+        }
+    }
+}
