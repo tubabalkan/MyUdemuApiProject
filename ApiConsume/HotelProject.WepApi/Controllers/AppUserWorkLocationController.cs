6fc1baa46e2b91783d6c29f013b31887d40bc777
tuba balkan <tubabalkan165@gmail.com>
2024-09-26 21:41:16 +0300

    ....

---
+++ ApiConsume/HotelProject.WepApi/Controllers/AppUserWorkLocationController.cs
@@ -0,0 +1,42 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.WepApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelProject.WepApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppUserWorkLocationController : ControllerBase
+    {
+        private readonly IAppUserService _appUserService;
+
+        public AppUserWorkLocationController(IAppUserService appUserService)
+        {
+            _appUserService = appUserService;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //var values=_appUserService.TUsersListWithWorkLocations();
+            Context context = new Context();
+            var values = context.Users.Include(x => x.WorkLocation).Select(y => new AppUserWorkLocationViewModel
+            {
+                Name = y.Name,
+                Surname = y.Surname,
+                WorkLocationID = y.WorkLocationID,
+                WorkLocationName = y.WorkLocation.WorkLocationName,
+                City=y.City,
+                ImageUrl=y.ImageUrl,
+                Email=y.Email,
+                UserName=y.UserName
+
+
+            }).ToList();
+;
+            return Ok(values);
+        }
+    }
+}
---
+++ ApiConsume/HotelProject.WepApi/Models/AppUserWorkLocationViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace HotelProject.WepApi.Models
+{
+    public class AppUserWorkLocationViewModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string ImageUrl { get; set; }
+        public string City { get; set; }
+        public string WorkLocationName { get; set; }
+        public int WorkLocationID { get; set; }
+    }
+}
