ec13bf65175a25b97f921b1c77ea0f1a0a75cee1
tuba balkan <tubabalkan165@gmail.com>
2024-10-04 15:36:59 +0300

    ....

---
+++ Frontend/HotelProject.WebUI/Controllers/RoleAssingController.cs
@@ -0,0 +1,63 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Models.Role;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class RoleAssingController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public RoleAssingController(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var values= _userManager.Users.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public async Task<IActionResult> AssingRole(int id)
+        {
+            var user=_userManager.Users.FirstOrDefault(x => x.Id == id);
+            TempData["userid"] = user.Id;
+            var roles=_roleManager.Roles.ToList();
+            var userRoles = await _userManager.GetRolesAsync(user);
+            List<RoleAssingViewModel> roleAssingViewModels = new List<RoleAssingViewModel>();
+            foreach (var item in roles)
+            {
+                RoleAssingViewModel model=new RoleAssingViewModel();
+                model.RoleId = item.Id;
+                model.RoleName = item.Name;
+                model.RoleExist=userRoles.Contains(item.Name);
+                roleAssingViewModels.Add(model);
+            }
+            
+            return View(roleAssingViewModels);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AssingRole(List<RoleAssingViewModel> roleAssingViewModel)
+        {
+            var userid = (int)TempData["userid"];
+            var user = _userManager.Users.FirstOrDefault(x => x.Id == userid);
+            foreach(var item in roleAssingViewModel)
+            {
+                if(item.RoleExist)
+                {
+                    await _userManager.AddToRoleAsync(user, item.RoleName);
+
+                }
+                else
+                {
+                    await _userManager.RemoveFromRoleAsync(user, item.RoleName);
+                }
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
